40ed25c8cf2eab38dfa565e5f36e34bd92c75223
BuildTools <unconfigured@null.spigotmc.org>
2020-12-23 20:07:40 +0100

    :sparkles: Added en_US support

---
+++ translations/en.py
@@ -0,0 +1,16 @@
+en = {
+    "weekDays": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    "months":
+        {
+            "abbreviated": ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+            "full": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                     "November", "December"]
+        },
+    "years": "Years",
+    "year": "Year",
+    "month": "Month",
+    "week": "Week",
+    "day": "Day",
+    "today": "Today",
+    "dateFormat": "%m %d, %Y"
+}
