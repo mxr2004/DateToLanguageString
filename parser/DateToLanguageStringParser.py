db77cda437425d95d7f20601c2f27482a90e3458
BuildTools <unconfigured@null.spigotmc.org>
2020-12-24 11:50:37 +0100

    :bug: fixed the parser

--- parser/DateToLanguageStringParser.py
+++ parser/DateToLanguageStringParser.py
@@ -7,7 +7,7 @@ from ..utils import language
 def convertDateTimeToString(dateTime: datetime, outputLanguage, alwaysShowDate=False):
     if not alwaysShowDate:
         today = datetime.utcnow()
-        dateTime = dateTime + timedelta(days=1)
+        dateTime = dateTime - timedelta(days=1)
         delta: timedelta = today - dateTime
         if abs(delta.days / 30) >= 1:
             if abs(delta.days / 30) == 1:
